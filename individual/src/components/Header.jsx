7827456e478ee1821a2e7ba60da98fdaca8b5dfd
jmanuelfrias <josemanuel.frias572@comunidadunir.net>
2023-12-05 00:56:42 +0100

    Añadidos header, footer y context. Intento de hook para traerme peliculas pero sigue con implementación de ejemplo

---
+++ individual/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () =>{
+    return (
+        <header className="header">
+            <h1 className="header-text">UNIR STREAMING</h1>
+        </header>
+    );
+}
